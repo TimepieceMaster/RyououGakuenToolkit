912e5ff07361bd8ff2c99897353fb975f937e09e
TimepieceMaster <masteroftimepieces@gmail.com>
2024-10-12 23:23:22 -0500

    Disable install option on main menu

--- apps/RGT_RGO_Patch_Builder/hdr/image_info/union_2528_1.h
+++ apps/RGT_RGO_Patch_Builder/hdr/image_info/union_2528_1.h
@@ -28,7 +28,7 @@ static texture_region_info s_union_2521_1_regions_data[] =
 		.read_top_left_offset = 0xFF8A4,
 		.read_top_left = { 332, 35 },
 		.write_top_left_offset = 0x103A48,
-		.write_top_left = { 205, 128 },
+		.write_top_left = { 205, 134 },
 		.read_size_offset = 0xFE580,
 		.read_size = { 71, 18 },
 		.write_size_offset = 0xFE584,
@@ -40,7 +40,7 @@ static texture_region_info s_union_2521_1_regions_data[] =
 		.read_top_left_offset = 0xFF8A8,
 		.read_top_left = { 332, 18 },
 		.write_top_left_offset = 0x103A4C,
-		.write_top_left = { 206, 152 },
+		.write_top_left = { 206, 161 },
 		.read_size_offset = 0xFE588,
 		.read_size = { 70, 18 },
 		.write_size_offset = 0xFE58C,
@@ -52,7 +52,7 @@ static texture_region_info s_union_2521_1_regions_data[] =
 		.read_top_left_offset = 0xFF8AC,
 		.read_top_left = { 408, 36 },
 		.write_top_left_offset = 0x103A50,
-		.write_top_left = { 209, 176 },
+		.write_top_left = { 209, 188 },
 		.read_size_offset = 0xFE590,
 		.read_size = { 62, 18 },
 		.write_size_offset = 0xFE594,
@@ -64,14 +64,14 @@ static texture_region_info s_union_2521_1_regions_data[] =
 		.read_top_left_offset = 0xFF8B0,
 		.read_top_left = { 421, 19 },
 		.write_top_left_offset = 0x103A54,
-		.write_top_left = { 215, 200 },
+		.write_top_left = { 215, 215 },
 		.read_size_offset = 0xFE598,
 		.read_size = { 53, 16 },
 		.write_size_offset = 0xFE59C,
 		.write_size = { 53, 16 },
 	},
 
-	/* Install */
+	/* Install (disabled) */
 	{ 
 		.read_top_left_offset = 0xFF8B4,
 		.read_top_left = { 332, 53 },
@@ -97,6 +97,11 @@ static texture_region_info s_union_2521_1_regions_data[] =
 static single_instruction_patch s_union_2521_1_instruction_patches_data[] =
 {
 	{ 0x47344, 0x3405007C }, /* Logo screen X */
+	{ 0x472A8, 0x2AA40004 }, /* Don't draw install option */
+
+	/* Disable ability to select install. */
+	{ 0x4771C, 0x28840004 },
+	{ 0x47744, 0x34040003 }
 };
 
 texture_region_info_array g_union_2521_1_regions =
